c27b2524423903e66da0dd18d003501d488de504
Walid nurudin <walidnurudin47@gmail.com>
2020-09-10 10:15:04 +0700

    duarr

--- src/components/project.js
+++ src/components/project.js
@@ -7,7 +7,7 @@ export default function Data() {
             <ul className="Project-ul">
                 <li><a href="https://one-chat.netlify.app/">one chat</a> - realtime chat project from Pondok Programmer.</li>
                 <li><a href="https://info-coronavirus.netlify.app/">covid-19</a> - covid pandemic information.</li>
-                <li>simple crud - simple crud table app.</li>
+                <li><a href="https://vuegogo.web.app">simple crud</a> - simple crud app.</li>
             </ul>
         </div>
     )
