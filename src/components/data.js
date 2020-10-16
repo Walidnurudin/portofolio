e23544776b00ee71502f1633fda09eca3012d637
Walidnurudin <walidnurudin47@gmail.com>
2020-10-17 00:46:53 +0700

    add new project

--- src/components/data.js
+++ src/components/data.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Data() {
     return (
         <div className="Data-div">
-            <h2>@walidnurudin</h2>
+            <div>
+                <h2>@walidnurudin</h2>
+            </div>
             <p>
                 I'm a frontend developer live in Indramayu, I spend most of my day learning fullstack developers. And I'm also a football fan.
             </p>
--- src/components/project.js
+++ src/components/project.js
@@ -8,6 +8,7 @@ export default function Data() {
                 <li><a href="https://one-chat.netlify.app/">one chat</a> - realtime chat project from Pondok Programmer.</li>
                 <li><a href="https://info-coronavirus.netlify.app/">covid-19</a> - covid pandemic information.</li>
                 <li><a href="https://vuegogo.web.app">simple crud</a> - simple crud app.</li>
+                <li><a href="https://mini-blogs-app.herokuapp.com/">mini-blogs</a> - mini blogs app.</li>
             </ul>
         </div>
     )
