6a16a5c6eed590ba000774e7e679a20b5a1ece14
Walidnurudin <walidnurudin47@gmail.com>
2020-11-15 22:59:12 +0700

    add project

--- src/components/project.js
+++ src/components/project.js
@@ -6,10 +6,11 @@ export default function Data() {
             <h2>project :</h2>
             <ul className="Project-ul">
                 <li><a href="https://one-chat.netlify.app/">one chat</a> - realtime chat project from Pondok Programmer.</li>
-                <li><a href="https://info-coronavirus.netlify.app/">covid-19</a> - covid pandemic information.</li>
+                <li><a href="https://info-coronavirus.netlify.app/">covid19</a> - covid pandemic information.</li>
                 <li><a href="https://vuegogo.web.app/">simple crud</a> - simple crud app.</li>
                 <li><a href="https://mini-blogs-app.herokuapp.com/">mini-blogs</a> - mini blogs app.</li>
-                <li><a href="https://praxisacademy.netlify.app/">praxis academy</a> - web bootcamp register</li>
+                <li><a href="https://salamdoc.id/">salamdoc</a> - web salamdoc.</li>
+                <li><a href="https://praxisacademy.netlify.app/">praxis academy</a> - web bootcamp register.</li>
                 <li><a href="https://admin-praxis.netlify.app/">admin praxis</a> - praxis system admin.</li>
             </ul>
         </div>
