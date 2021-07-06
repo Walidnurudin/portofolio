e15e4c599e3a94566dc45cec9446802b32fba476
Walidnurudin <walidnurudin47@gmail.com>
2021-07-06 11:51:47 +0700

    add project

--- src/components/project.js
+++ src/components/project.js
@@ -5,13 +5,14 @@ export default function Data() {
         <div className="Project-div">
             <h2>project :</h2>
             <ul className="Project-ul">
-                <li><a href="https://one-chat.netlify.app/">one chat</a> - realtime chat project from Pondok Programmer.</li>
-                <li><a href="https://info-coronavirus.netlify.app/">covid19</a> - covid pandemic information.</li>
-                <li><a href="https://vuegogo.web.app/">simple crud</a> - simple crud app.</li>
-                <li><a href="https://mini-blogs-app.herokuapp.com/">mini-blogs</a> - mini blogs app.</li>
-                <li><a href="https://salamdoc.id/">salamdoc</a> - web salamdoc.</li>
-                <li><a href="https://praxisacademy.netlify.app/">praxis academy</a> - web bootcamp register.</li>
-                <li><a href="https://admin-praxis.netlify.app/">admin praxis</a> - praxis system admin.</li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://one-chat.netlify.app/">one chat</a> - realtime chat project from Pondok Programmer.</li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://info-coronavirus.netlify.app/">covid19</a> - covid pandemic information.</li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://vuegogo.web.app/">simple crud</a> - simple crud app.</li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://mini-blogs-app.herokuapp.com/">mini-blogs</a> - mini blogs app.</li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://salamdoc.id/">salamdoc</a> - web salamdoc.</li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://praxisacademy.netlify.app/">praxis academy</a> - web bootcamp register.</li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://admin-praxis.netlify.app/">admin praxis</a> - praxis admin system.</li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://admin-staycation-apps.herokuapp.com/">admin staycation</a> - staycation admin system from BWA.</li>
             </ul>
         </div>
     )
