af80f0746f16475acf01ff85d1bdba1f156a29e1
Walidnurudin <walidnurudin47@gmail.com>
2022-01-03 17:19:42 +0700

    add assets, instal react-multi-carousel, update component project

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,19 +1,37 @@
-import React from 'react'
-import { MdEmail } from "react-icons/md"
-import { FaGithub, FaTelegram, FaHeart } from "react-icons/fa"
-import { AiFillLinkedin } from "react-icons/ai"
+import React from "react";
+import { MdEmail } from "react-icons/md";
+import { FaGithub, FaTelegram, FaHeart } from "react-icons/fa";
+import { AiFillLinkedin } from "react-icons/ai";
 
 export default function Contact() {
-    return (
-        <div className="Contact-div">
-            <h2>contact :</h2>
-            <ul className="Contact-ul">
-                <li><a href="https://t.me/WalidNurudin"><FaTelegram /> telegram</a></li>
-                <li><a href="https://github.com/Walidnurudin"><FaGithub /> github</a></li>
-                <li><a href="https://linkedin.com/in/walidnurudin"><AiFillLinkedin /> linked</a></li>
-                <li><a href="mailto:walidnurudin47@gmail.com"><MdEmail /> email</a></li>
-            </ul>
-            <div className="Footer"><FaHeart /></div>
-        </div>
-    )
+  return (
+    <div className="Contact-div">
+      <h2>contact :</h2>
+      <ul className="Contact-ul">
+        <li>
+          <a href="https://t.me/WalidNurudin">
+            <FaTelegram /> telegram
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Walidnurudin">
+            <FaGithub /> github
+          </a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/in/walidnurudin">
+            <AiFillLinkedin /> linkedin
+          </a>
+        </li>
+        <li>
+          <a href="mailto:walidnurudin47@gmail.com">
+            <MdEmail /> email
+          </a>
+        </li>
+      </ul>
+      <div className="Footer">
+        <FaHeart />
+      </div>
+    </div>
+  );
 }
--- src/components/project.js
+++ src/components/project.js
@@ -1,19 +1,181 @@
-import React from 'react'
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { FaGithub, FaGlobe } from "react-icons/fa";
+import {
+  homeTickitz,
+  loginTickitz,
+  movieDetailTickitz,
+  orderTickitz,
+} from "../assets/images";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 1,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+const image = [
+  {
+    title: "login page",
+    image: loginTickitz,
+  },
+  {
+    title: "home page",
+    image: homeTickitz,
+  },
+  {
+    title: "movie detail page",
+    image: movieDetailTickitz,
+  },
+  {
+    title: "order page",
+    image: orderTickitz,
+  },
+];
 
 export default function Data() {
-    return (
-        <div className="Project-div">
-            <h2>project :</h2>
-            <ul className="Project-ul">
-                <li><a target="_blank" rel="noopener noreferrer" href="https://one-chat.netlify.app/">one chat</a> - realtime chat project from Pondok Programmer.</li>
-                <li><a target="_blank" rel="noopener noreferrer" href="https://info-coronavirus.netlify.app/">covid19</a> - covid pandemic information.</li>
-                <li><a target="_blank" rel="noopener noreferrer" href="https://vuegogo.web.app/">simple crud</a> - simple crud app.</li>
-                <li><a target="_blank" rel="noopener noreferrer" href="https://mini-blogs-app.herokuapp.com/">mini-blogs</a> - mini blogs app.</li>
-                <li><a target="_blank" rel="noopener noreferrer" href="https://salamdoc.id/">salamdoc</a> - web salamdoc.</li>
-                <li><a target="_blank" rel="noopener noreferrer" href="https://praxisacademy.netlify.app/">praxis academy</a> - web bootcamp register.</li>
-                <li><a target="_blank" rel="noopener noreferrer" href="https://admin-praxis.netlify.app/">admin praxis</a> - praxis admin system.</li>
-                <li><a target="_blank" rel="noopener noreferrer" href="https://admin-staycation-apps.herokuapp.com/">admin staycation</a> - staycation admin system from BWA.</li>
-            </ul>
+  return (
+    <div>
+      <h2>Zwallet</h2>
+
+      <Carousel responsive={responsive}>
+        {image.map((item, index) => (
+          <img
+            src={item.image}
+            aria-hidden
+            alt={item.title}
+            key={index}
+            width="100%"
+          />
+        ))}
+      </Carousel>
+
+      <p>
+        Zwallet adalah aplikasi transaksi yang bisa kamu pakai untuk melakukan
+        top up dan transfer ke sesama pengguna aplikasi zwallet. Zwallet
+        dibangun menggunakan NextJS.
+      </p>
+
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <div>
+          <FaGithub />
+          <span>https://zwallet-x.vercel.app/</span>
         </div>
-    )
+
+        <div>
+          <FaGlobe />
+          <span>https://github.com/Walidnurudin/zwallet</span>
+        </div>
+      </div>
+
+      {/* <ul className="Project-ul">
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://zwallet-x.vercel.app/"
+          >
+            zwallet
+          </a>{" "}
+          - Zwallet adalah aplikasi transaksi yang bisa kamu pakai untuk
+          melakukan top up dan transfer ke sesama pengguna aplikasi zwallet.
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://one-chat.netlify.app/"
+          >
+            one chat
+          </a>{" "}
+          - realtime chat project from Pondok Programmer.
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://info-coronavirus.netlify.app/"
+          >
+            covid19
+          </a>{" "}
+          - covid pandemic information.
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://vuegogo.web.app/"
+          >
+            simple crud
+          </a>{" "}
+          - simple crud app.
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://mini-blogs-app.herokuapp.com/"
+          >
+            mini-blogs
+          </a>{" "}
+          - mini blogs app.
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://salamdoc.id/"
+          >
+            salamdoc
+          </a>{" "}
+          - web salamdoc.
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://praxisacademy.netlify.app/"
+          >
+            praxis academy
+          </a>{" "}
+          - web bootcamp register.
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://admin-praxis.netlify.app/"
+          >
+            admin praxis
+          </a>{" "}
+          - praxis admin system.
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://admin-staycation-apps.herokuapp.com/"
+          >
+            admin staycation
+          </a>{" "}
+          - staycation admin system from BWA.
+        </li>
+      </ul> */}
+    </div>
+  );
 }
