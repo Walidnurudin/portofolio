79b75610ea797264d454e6f88478bb10241df86f
walidnurudin <walidnurudin47@gmail.com>
2022-04-24 23:51:17 +0700

    init new project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import Home from "./containers/home"
+import React, { Component } from 'react';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { FaGithub, FaHeart } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="Content">
-        <Home />
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>contact :</h2>
+        <ul>
+          <li>
+            <a href='https://linkedin.com/in/walidnurudin'>
+              <AiFillLinkedin /> linkedin
+            </a>
+          </li>
+          <li>
+            <a href='https://github.com/Walidnurudin'>
+              <FaGithub /> github
+            </a>
+          </li>
+
+          <li>
+            <a href='mailto:walidnurudin47@gmail.com'>
+              <MdEmail /> email
+            </a>
+          </li>
+        </ul>
+        <div>
+          <FaHeart />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
