dabc44d4bb6c3c6544ff49f9f61457c76746a130
Walidnurudin <walidnurudin47@gmail.com>
2023-06-08 10:58:58 +0700

    :zap: set title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,10 +5,15 @@ import Main from '@/layouts/main';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
+      <Head>
+        <title>Walid Nurudin</title>
+      </Head>
+
       <Main>
         <Component {...pageProps} />
         <ToastContainer />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta property="og:title" content="Walid Nurudin" key="title" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
